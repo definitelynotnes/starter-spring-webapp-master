2146b0d86966d3b0921123cd8ab28643b20f3f52
definitelynotnes <nestorjohntenorio@gmail.com>
2022-11-15 16:11:08 +0800

    added

--- src/main/java/com/bootcamp/web/service/UserRegistrationService.java
+++ src/main/java/com/bootcamp/web/service/UserRegistrationService.java
@@ -1,25 +1,8 @@
 package com.bootcamp.web.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.bootcamp.web.entity.User;
-import com.bootcamp.web.repository.UserRepository;
 
-@Service
-public class UserRegistrationService {
-    @Autowired
-    private UserRepository userRepository;
-
-    public void register(User user) {
-        if (user == null) {
-            throw new IllegalArgumentException("User cannot be null");
-        }
-
-        if (userRepository.findByEmail(user.getEmail()) != null) {
-            throw new RuntimeException("Email address taken.");
-        }
-
-        userRepository.save(user);
-    }
+public interface UserRegistrationService {
+    
+    void register(User user);
 }
